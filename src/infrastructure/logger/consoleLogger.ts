2172199e845aff6797fac7d8d3202fd3b5c9c21c
Ahmad Abdalraheem <ahmad.abdalraheem@optimumpartners.co>
2025-01-17 08:32:55 +0300

    updat logger, fix undefind bug for path

--- src/infrastructure/logger/consoleLogger.ts
+++ src/infrastructure/logger/consoleLogger.ts
@@ -1,38 +1,38 @@
 import colors from './colors';
 
 class Logger {
-  public static Error(data: any, path?: string): void {
+  public static Error(data: any, path: string = ''): void {
     console.log(
       colors.redBg,
       'Error',
       colors.reset,
       colors.red,
       `${path && colors.red}`,
-      path ? ` occurred in ${path} ` : ' ',
+      (path ? ` occurred in ${path} ` : ''),
       colors.reset,
       data,
     );
   }
 
-  public static Warn(data: any, path?: string): void {
+  public static Warn(data: any, path: string = ''): void {
     console.log(
       colors.yellowBg,
       'Warn',
       colors.yellow,
       `${path && colors.red}`,
-      path ? ` related to ${path} ` : ' ',
+      path ? ` related to ${path} ` : '',
       colors.reset,
       data,
     );
   }
 
-  public static Info(data: any, path?: string): void {
+  public static Info(data: any, path: string = ''): void {
     console.log(
       colors.blueBg,
       'Info',
       colors.reset + colors.blue,
       `${path && colors.blue}`,
-      path ? ` related to ${path} ` : ' ',
+      path ? ` related to ${path} ` : '',
       colors.reset,
       data,
     );
